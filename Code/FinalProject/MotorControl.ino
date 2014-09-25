376a8bab9236ac7d5e94bc56c0f9755a0d152b05
DrVulpinus <siganus.vulpinus@live.com>
2014-09-25 15:09:31 -0400

    Added Stop Motors Command

--- Code/FinalProject/MotorControl.ino
+++ Code/FinalProject/MotorControl.ino
@@ -16,3 +16,8 @@ void runMotors(int leftPower, int rightPower){
   rightDrive.write(map(rightPower,-100,100,0,180)); //map the input from a scale of -100 to 100 for ease of use, and then write it to the motor.
   leftDrive.write(map(leftPower,-100,100, 180,0)); //map the input from a scale of -100 to 100 for ease of use, and then write it to the motor.
 }
+
+void stopMotors(){
+   rightDrive.write(90); //Stop the Right Motor
+  leftDrive.write(90); //Stop the left Motor
+}
